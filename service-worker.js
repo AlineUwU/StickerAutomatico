72081f858d40593717bb55bbed9342288afb16fa
AlineUwU <132239125+AlineUwU@users.noreply.github.com>
2025-06-28 19:55:37 -0600

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'stickers-cache-v1';
+const FILES_TO_CACHE = [
+  'StickerAutomatico/',
+  'StickerAutomatico/index.html',
+  'StickerAutomatico/styles.css',
+  'StickerAutomatico/main.js',
+  'StickerAutomatico/manifest.json',
+  'StickerAutomatico/icon-192.png',
+  'StickerAutomatico/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
+});
