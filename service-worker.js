ccd00648cdc96d18a30e737e72bd31ac8927a60d
AlineUwU <132239125+AlineUwU@users.noreply.github.com>
2025-06-30 01:06:43 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@ const FILES_TO_CACHE = [
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache).catch(error => {
+      return cache.addAll(FILES_TO_CACHE)
         console.error('❌ Error cacheando archivos:', error);
       });
     })
